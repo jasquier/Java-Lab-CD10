3bf38de59df24d3469d75c5c43380464be5e6ed7
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-01-12 16:35:45 -0500

    Added more unit tests

--- src/main/java/squier/john/javacd10/InputSummer.java
+++ src/main/java/squier/john/javacd10/InputSummer.java
@@ -7,6 +7,11 @@ public class InputSummer {
 
     public int sumUpToInput(int input) {
 
+        // Since the specs we're unclear
+        if ( input < 0) {
+            return -1;
+        }
+
         int sum = 0;
 
         for ( int i = 1; i <= input; i++ ) {
